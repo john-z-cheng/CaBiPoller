9cddd08d145e20dfe03e557efb2a686351bab85f
John Cheng <john.z.cheng@gmail.com>
2016-11-26 20:12:56 -0500

    add jurisdiction grouping for broken dock stations

--- stations/views.py
+++ stations/views.py
@@ -2,17 +2,44 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 from django.db.models import F
+
+from collections import defaultdict
 from stations.models import Station
+from stations.models import RefStation
 from datetime import datetime
 from additional import cabi_status
 
+def create_jurisdiction_data():
+	refStationAry = RefStation.objects.all()
+	stationToJurisdiction = {}
+	for ref in refStationAry:
+		stationToJurisdiction[ref.id] = ref.jurisdiction
+	return stationToJurisdiction
+	
+def get_list_per_jurisdiction(stationToJurisdiction, stationList):
+	stationsByJurisdiction = defaultdict(list)
+	for station in stationList:
+		jurisdiction = stationToJurisdiction[station.id]
+		stationsByJurisdiction[jurisdiction].append(station)
+	# convert to list of tuples (jurisdiction, list) ordered by name
+	names =  sorted(stationsByJurisdiction.keys())
+	jurisList = list()
+	for name in names:
+		subList = stationsByJurisdiction[name]
+		jurisList.append((name, subList))
+	return jurisList
+	
 def index(request):
 	stations = {}
+	stationToJurisdiction = create_jurisdiction_data()
 	brokenList = Station.objects.filter(curr_total__lt=F('max_total'))
 	# add attribute for difference
 	for broken in brokenList:
 		broken.difference = broken.max_total - broken.curr_total
-	stations['broken'] = brokenList
+		broken.place = 'Blue '
+	jurisList = get_list_per_jurisdiction(stationToJurisdiction, brokenList)
+
+	stations['broken'] = jurisList
 	stations['nobikes'] = Station.objects.filter(bikes=0)
 	stations['nodocks'] = Station.objects.filter(docks=0)
 	timestamp = Station.objects.get(id=31000).poll_time
@@ -27,7 +54,9 @@ def index(request):
 def station_detail(request, id):
 	try:
 		station = Station.objects.get(id=id)
-	except Station.DoesNotExist:
+		refStation = RefStation.objects.get(id=id)
+		station.jurisdiction = refStation.jurisdiction
+	except (Station.DoesNotExist, RefStation.DoesNotExist):
 		raise Http404('This station does not exist')
 	return render(request, 'status/station_detail.html', {
 		'station': station
